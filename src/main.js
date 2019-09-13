32457b681fea549a8003b7059d46cbba8569aefa
Danilo3110 <lukic3@sbb.rs>
2019-09-13 16:38:36 +0200

    added navigation bar, and small changes in CSS, added effects

--- src/main.js
+++ src/main.js
@@ -5,7 +5,19 @@ const animateFocus = (toLocation) => {
 };
 
 $(document).ready(() => {
-    $('#title').on('click', () => animateFocus('.logo1'));
+    $('#site_logo, #home').on('click', () => animateFocus('.main'));
+    $('#about').on('click', () => animateFocus('.main_info'));
+    $('#ingredients').on('click', () => animateFocus('.logo1'));
+    $('#delivery').on('click', () => animateFocus('.box_usage'));
+    $('#title, #order').on('click', () => animateFocus('.logo2'));
     $('#price_tag').on('click', () => animateFocus('.form_title'));
     $('.desc_button').on('click', () => animateFocus('.logo2'));
+});
+
+$(document).ready(() => {
+    $(window).scroll(() => {
+        return $(window).scrollTop() > 80 ?
+            $('.box').css({backgroundColor: 'rgba(13, 23, 37, 0.9)', backgroundImage: 'none'}) :
+            $('.box').css({backgroundColor: 'rgba(0, 0, 0, 0)', backgroundImage: 'linear-gradient(360deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.59) 100%)'});
+    });
 });
