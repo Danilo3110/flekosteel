773e2903ebbe8799bf56deeef75974324f10f3db
Danilo3110 <lukic3@sbb.rs>
2019-09-14 14:21:55 +0200

    adedd axios, mySQL, and express.js for backend

---
+++ backend/config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    dbConfig: {
+        host: 'remotemysql.com',
+        user: 'ZxZT8xD0Id',
+        password: '0yuMXjJhJz',
+        database: 'ZxZT8xD0Id',
+        connectionLimit: 100,
+        debug: false
+    }
+};
---
+++ backend/routes/route.js
@@ -0,0 +1,25 @@
+
+module.exports = function (app, express, mysqlConnection) {
+
+    const router = express.Router();
+  
+    // podaci table //
+    router.route('/narudzbine')
+      .get((req, res) => {
+        mysqlConnection.query('SELECT * FROM narudzbine', function (error, results) {
+          if (error) throw error;
+          return res.send({ error: false, data: results, message: 'narudzbine - lista', user: req.user });
+        });
+      })
+      .post((req, res) => {
+        if (!req.body) {
+          return res.status(400).send({ error: true, message: 'Please provide narudzbine data' });
+        }
+        mysqlConnection.query('INSERT INTO narudzbine (ime_prezime, adresa, telefon, datum) values (?,?,?,?)',
+          [req.body.ime_prezime, req.body.adresa, req.body.telefon, req.body.datum], function (error, results) {
+            if (error) throw error;
+            //req.user is the info about the logged in user
+            return res.send({ error: false, data: results, message: 'New list has been added successfully.', user: req.user });
+          });
+      });
+}
